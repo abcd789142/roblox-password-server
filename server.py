b18badbabf59e4a2ca5e7e9e83dfae5e49b05b37
abcd789142 <abcd145365@outlook.com>
2025-06-20 20:40:30 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,17 +1,19 @@
 from flask import Flask, request
+     from serverless_wsgi import handle_request
 
-app = Flask(__name__)
-password = "123456"  # 初始密码
+     app = Flask(__name__)
+     password = "123456"  # 初始密码
 
-@app.route('/password', methods=['GET'])
-def get_password():
-    return password
+     @app.route('/password', methods=['GET'])
+     def get_password():
+         return password
 
-@app.route('/password', methods=['POST'])
-def set_password():
-    global password
-    password = request.data.decode('utf-8')
-    return "Password updated"
+     @app.route('/password', methods=['POST'])
+     def set_password():
+         global password
+         password = request.data.decode('utf-8')
+         return "Password updated"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+     # Vercel serverless 入口
+     def handler(event, context):
+         return handle_request(app, event, context)
